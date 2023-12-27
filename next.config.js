e737b262a2ad8ed0766465299a76333d7e14c216
youssef097 <youssef.webdev12@gmail.com>
2023-12-27 23:30:42 +0100

    fix: cors settings

--- next.config.js
+++ next.config.js
@@ -7,6 +7,29 @@ const nextConfig = {
       permanent: true,
     },
   ],
+
+  headers: async () => {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
+
   env: {
     PROJECT_ID: process.env.PROJECT_ID,
     PROJECT_NAME: process.env.PROJECT_NAME,
